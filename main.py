0db8e87d542d9aa82ad6269ffc64f4f4139965f6
faizan2700 <syedfaizan824@gmail.com>
2024-12-17 14:02:27 +0530

    updated app to be simple echo

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import os
 import uvicorn
-from app import app 
+# from app import app 
+from fastapi import FastAPI, Request, Response 
+app = FastAPI(title='Whatsapp Bot Application') 
+
+@app.get('/') 
+async def home(request: Request): 
+    return Response('<h1>Hello World</h1>', status_code=200) 
+
+@app.post('/') 
+async def create_something(request: Request): 
+    payload = await request.json() 
+    username = payload.get('username', 'none') 
+    email = payload.get('email', 'none') 
+    data = {'usernmae': username, 'email': email} 
+    return data
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
