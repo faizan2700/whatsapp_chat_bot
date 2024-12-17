3bf174fb33e769e728b5e5ae904191c22578c084
faizan2700 <syedfaizan824@gmail.com>
2024-12-17 19:28:36 +0530

    updated requirements and all changes

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import requests
 from dotenv import load_dotenv 
 from whatsapp_service import WhatsAppService 
 from ai_agent import Assistant  
-from main import app 
+
+
 
 
 # Load environment variables
 load_dotenv('./.env') 
 # Create FastAPI app instance
-app1 = FastAPI(title="WhatsApp Echo Bot")
+app = FastAPI(title="WhatsApp Echo Bot")
 
 # Initialize WhatsApp service
 whatsapp_service = WhatsAppService()
 ai_agent = Assistant() 
-@app1.post("/webhook")
+@app.post("/webhook")
 async def handle_webhook(request: Request):
     body = request.json() 
     number, message = whatsapp_service.get_message(body)  
@@ -23,7 +24,7 @@ async def handle_webhook(request: Request):
     response = whatsapp_service.send_message(number, message) 
     return {"status": "success", "response": response} 
 
-@app1.get("/webhook") 
+@app.get("/webhook") 
 async def verify_webhook(request: Request):
     mode = request.query_params.get("hub.mode")
     token = request.query_params.get("hub.verify_token")
@@ -35,13 +36,17 @@ async def verify_webhook(request: Request):
 
 
 
-@app1.post("/send_message") 
+@app.post("/send_message") 
 async def send_message(message_request: Request):
     body = await message_request.json() 
     message = body.get("message") 
     to_number = body.get("to_number") 
     response = whatsapp_service.send_message(to_number, message) 
     return { "status": "success", "response": response }
+
+@app.get('/home') 
+async def home(request: Request): 
+    return {'message': 'Hello World'} 
 
 # Main entry point for running the application
 if __name__ == "__main__":
